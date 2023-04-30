740a6046dd546c43d32f6862db56c4bf78f376e3
troner14 <bardeliger@gmail.com>
2023-04-30 13:02:41 +0200

    Update locales

--- locales/en.lua
+++ locales/en.lua
@@ -1,4 +1,5 @@
 Locales['en'] = {
+    --  client locate
     ['input.select.label'] = 'report type',
     ['input.textarea.label'] = 'Description',
     ['input.textarea.placeholder'] = 'what happens',
@@ -8,4 +9,15 @@ Locales['en'] = {
     ['menu.reporte.options.teleport'] = 'teleport to player',
     ['menu.reporte.options.Discordmsg'] = 'send notify to go discord',
     ['menu.reporte.options.closeMenu'] = 'Close menu',
+
+    -- server locate
+    ['ox_lib.notify.title'] = 'Report',
+    ['ox_lib.notify.newReport.description'] = 'report sent successfully',
+    ['ox_lib.notify.closeReport.description'] = 'report deleted successfully',
+    ['ox_lib.notify.teleport.notteleporttoyourself'] = 'you can\'t teleport yourself.',
+    ['ox_lib.notify.teleport.xplayermessage'] = 'you teleported to %s',
+    ['ox_lib.notify.teleport.tplayermessage'] = '%s has been teleported to you.',
+    ['ox_lib.notify.DiscSend.description'] = 'the administrator %s It asks you in the discord support room to attend to your report.',
+    ['command.togglereport.description.true'] = 'You just turned on report notifications',
+    ['command.togglereport.description.false'] = 'You just turned off report notifications',
 }
--- locales/es.lua
+++ locales/es.lua
@@ -1,4 +1,5 @@
 Locales['es'] = {
+    --  client locate
     ['input.select.label'] = 'tipo de reporte',
     ['input.textarea.label'] = 'Descripcion',
     ['input.textarea.placeholder'] = 'Que ha pasado',
@@ -8,4 +9,16 @@ Locales['es'] = {
     ['menu.reporte.options.teleport'] = 'teletrasportarte al jugador',
     ['menu.reporte.options.Discordmsg'] = 'pedir que venga a disc',
     ['menu.reporte.options.closeMenu'] = 'cerrar menu',
+
+    -- server locate
+    ['ox_lib.notify.title'] = 'Reporte',
+    ['ox_lib.notify.newReport.description'] = 'reporte enviado de manera existosa',
+    ['ox_lib.notify.closeReport.description'] = 'reporte eliminado de manera existosa',
+    ['ox_lib.notify.teleport.notteleporttoyourself'] = 'no puedes tepearte a ti mismo.',
+    ['ox_lib.notify.teleport.xplayermessage'] = 'te teletrasportaste a %s',
+    ['ox_lib.notify.teleport.tplayermessage'] = '%s a sido teletrasportado hacia ti.',
+    ['ox_lib.notify.DiscSend.description'] = 'el  administrador %s te pide en sala de soporte de discord para atender tu reporte.',
+    ['command.togglereport.description.true'] = 'Usted acava de activar las notificaciones de reportes',
+    ['command.togglereport.description.false'] = 'Usted acava de desactivar las notificaciones de reportes',
+
 }
