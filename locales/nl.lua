bac7d03c23a304926b5bfbe5e90eaf6752fd6b0c
troner14 <73949396+troner14@users.noreply.github.com>
2023-06-02 23:01:21 +0200

    add duch (nl) locales maked by Patrick Wajong#2893

---
+++ locales/nl.lua
@@ -0,0 +1,26 @@
+Locales['nl'] = {
+    --  client locate
+    ['input.select.label'] = 'Report Type',
+    ['input.textarea.label'] = 'Beschrijving',
+    ['input.textarea.placeholder'] = 'Wat is er aan de hand?',
+    ['menu.reportes.title'] = 'Report Menu',
+    ['menu.reporte.title'] = 'Report %s',
+    ['menu.reporte.options.closeReport'] = 'Opgelost',
+    ['menu.reporte.options.teleport'] = 'Teleporteer naar burger',
+    ['menu.reporte.options.Discordmsg'] = 'Ga a.u.b. naar de wachtkamer',
+    ['menu.reporte.options.closeMenu'] = 'Sluit menu',
+    ['alert.header'] = 'Nieuw Report',
+    ['alert.content'] = 'Er is een nieuwe report gemaakt door %s',
+
+    -- server locate
+    ['ox_lib.notify.title'] = 'Report',
+    ['ox_lib.notify.newReport.description'] = 'Report is verzonden',
+    ['ox_lib.notify.closeReport.description'] = 'Report is verwijderd',
+    ['ox_lib.notify.teleport.notteleporttoyourself'] = 'Je kan jezelf niet teleporteren.',
+    ['ox_lib.notify.teleport.xplayermessage'] = 'Je bent geteleporteerd naar %s',
+    ['ox_lib.notify.teleport.tplayermessage'] = '%s is naar jou toegekomen.',
+    ['ox_lib.notify.DiscSend.description'] = 'De Admin %s komt naar de support chanel om je report te bespreken.',
+    ['command.togglereport.description.true'] = 'Report meldingen aangezet',
+    ['command.togglereport.description.false'] = 'Report meldingen uitgezet',
+    ['command.getReports.notify.notReports'] = 'Geen reports'
+}
